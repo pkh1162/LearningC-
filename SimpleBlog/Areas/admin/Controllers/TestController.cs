3e5c94814db9cbf4b00a0ad9189c1ba0e669b52c
pkh1162 <paulyk1162@hotmail.co.uk>
2016-05-14 16:16:46 +0100

    Updated admin pages (added navigation bar selected tab attributes)

--- SimpleBlog/Areas/admin/Controllers/TestController.cs
+++ SimpleBlog/Areas/admin/Controllers/TestController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleBlog.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +8,7 @@ using System.Web.Mvc;
 namespace SimpleBlog.Areas.Admin.Controllers
 {
     [Authorize(Roles = "admin")]
+    [SelectedTabAttribute("test")]
     public class TestController : Controller
     {
         // GET: Test/Admin
---
+++ SimpleBlog/Infrastructure/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlog.Infrastructure
+{
+    public class Extensions
+    {
+        public static string SelectedTabHelper(string viewBag, string selectedTab)
+        {
+            if (viewBag == selectedTab)
+            {
+                return "active";
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
